01f48d6902c0f34313984d0cc8d881d3aa5d6215
mark456tung <mark456tung@gmail.com>
2022-05-13 17:15:33 +0800

    branch test

--- OPTR/OPTR/views.py
+++ OPTR/OPTR/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'home.html',locals())
+
+#test branch
+def bracnhtest(request):
+    return render(request,'home.html',locals())
